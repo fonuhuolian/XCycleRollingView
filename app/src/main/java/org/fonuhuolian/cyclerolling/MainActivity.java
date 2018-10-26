9a5b594764f72e27df7602d6871a4e01d44bb190
fonuhuolian <25758058@qq.com>
2018-10-26 08:55:44 +0800

    1.0.0

---
+++ app/src/main/java/org/fonuhuolian/cyclerolling/MainActivity.java
@@ -0,0 +1,29 @@
+package org.fonuhuolian.cyclerolling;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import org.fonuhuolian.cyclerollingview.XCycleRollingView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        XCycleRollingView view = (XCycleRollingView) findViewById(R.id.xv);
+
+
+        for (int i = 0; i < 5; i++) {
+
+            View inflate = LayoutInflater.from(this).inflate(R.layout.a, null);
+
+            view.addItemView(inflate);
+        }
+
+        view.initialise();
+    }
+}
---
+++ cyclerollingview/src/main/java/org/fonuhuolian/cyclerollingview/XCycleRollingView.java
@@ -0,0 +1,66 @@
+package org.fonuhuolian.cyclerollingview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.ViewFlipper;
+
+public class XCycleRollingView extends RelativeLayout {
+
+
+    private boolean isAutoScroll = false;
+    private int autoScrollInterval = 2000;
+    private ViewFlipper flipper;
+
+    public XCycleRollingView(Context context) {
+        this(context, null);
+    }
+
+    public XCycleRollingView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public XCycleRollingView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        getAttrs(context, attrs);
+
+        LayoutInflater.from(context).inflate(R.layout.x_cycle_rolling_layout, this, true);
+        flipper = (ViewFlipper) findViewById(R.id.scrollView);
+    }
+
+    /**
+     * 得到属性值
+     *
+     * @param context
+     * @param attrs
+     */
+    private void getAttrs(Context context, AttributeSet attrs) {
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.XCycleRollingView);
+        autoScrollInterval = ta.getInteger(R.styleable.XCycleRollingView_autoScrollInterval, autoScrollInterval);
+        isAutoScroll = ta.getBoolean(R.styleable.XCycleRollingView_isAutoScroll, isAutoScroll);
+        ta.recycle();
+    }
+
+
+    public XCycleRollingView addItemView(View view) {
+        flipper.addView(view);
+        return this;
+    }
+
+
+    public XCycleRollingView initialise() {
+        flipper.setAutoStart(isAutoScroll);
+        flipper.setFlipInterval(autoScrollInterval);
+        return this;
+    }
+
+    public XCycleRollingView clearAllViews() {
+        flipper.removeAllViews();
+        return this;
+    }
+
+}
